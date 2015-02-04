aa276dfbd325f0575a3a51f2823d5f1742eb396d
Jens Melgaard <admin@it-links.dk>
2015-02-04 17:37:43 +0100

    Query Provider

---
+++ DotJEM.Json.Storage.Test/Linq/StorageAreaQueryableProviderTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DotJEM.Json.Storage.Linq;
+
+namespace DotJEM.Json.Storage.Test.Linq
+{
+    [TestFixture]
+    public class StorageAreaQueryableProviderTest
+    {
+        [Test]
+        public void Provide()
+        {
+            StorageAreaQ storage = new StorageAreaQ(new SqlConnection("Data Source=.\\DEV;Initial Catalog=json;Integrated Security=True"));
+
+
+            IQueryable<Entity> other = from entity in storage.Open("test")
+                where entity.ContentType == "dummy"
+                select entity;
+
+            Entity[] list = other.ToArray();
+
+        }
+
+    }
+}
--- DotJEM.Json.Storage/Linq/QueryableStorage.cs
+++ DotJEM.Json.Storage/Linq/QueryableStorage.cs
@@ -1,24 +1,91 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace DotJEM.Json.Storage.Linq
 {
-    public class StorageAreaContext : IOrderedQueryable<JObject>
+    //http://blogs.msdn.com/b/mattwar/archive/2008/11/18/linq-links.aspx
+    public class Entity
+    {
+        public Guid Id { get; private set; }
+        public string Reference { get; private set; }
+        public int Version { get; private set; }
+        public string ContentType { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime Updated { get; private set; }
+        public JObject Data { get; private set; }
+    }
+
+    public class StorageAreaQ
+    {
+        private DbConnection connection;
+
+        public Query<Entity> Open(string area)
+        {
+            return new Query<Entity>(new StorageAreaQueryProvider(connection, area));
+        }
+
+        public StorageAreaQ(DbConnection connection)
+        {
+            this.connection = connection;
+        }
+    }
+
+    public class StorageAreaQueryProvider : QueryProvider
+    {
+        readonly DbConnection connection;
+        private readonly string area;
+
+        public StorageAreaQueryProvider(DbConnection connection, string area)
+        {
+            this.connection = connection;
+            this.area = area;
+        }
+
+        public override string GetQueryText(Expression expression)
+        {
+            return Translate(expression);
+        }
+
+        public override object Execute(Expression expression)
+        {
+            DbCommand cmd = connection.CreateCommand();
+            cmd.CommandText = Translate(expression);
+            DbDataReader reader = cmd.ExecuteReader();
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+            return Activator.CreateInstance(
+                typeof(ObjectReader<>).MakeGenericType(elementType),
+                BindingFlags.Instance | BindingFlags.NonPublic, null,
+                new object[] { reader },
+                null);
+        }
+
+        private string Translate(Expression expression)
+        {
+            return new QueryTranslator(area).Translate(expression);
+        }
+    }
+
+
+    public class StorageAreaContext : IOrderedQueryable<Entity>
     {
         public StorageAreaContext(string area)
         {
             Provider = new StorageAreaQueryableProvider(area);
+            Expression = Expression.Constant(this);
         }
 
-        public IEnumerator<JObject> GetEnumerator()
+        public IEnumerator<Entity> GetEnumerator()
         {
-            yield return new JObject();
+            return Provider.Execute<IEnumerable<Entity>>(Expression).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -26,7 +93,11 @@ namespace DotJEM.Json.Storage.Linq
             return GetEnumerator();
         }
 
-        public Type ElementType { get; private set; }
+        public Type ElementType
+        {
+            get { return typeof(Entity); }
+        }
+
         public Expression Expression { get; private set; }
         public IQueryProvider Provider { get; private set; }
     }
@@ -47,7 +118,10 @@ namespace DotJEM.Json.Storage.Linq
 
         public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
         {
-            throw new NotImplementedException();
+            Debug.WriteLine(expression);
+
+
+            return new List<TElement>().AsQueryable();
         }
 
         public object Execute(Expression expression)
@@ -58,6 +132,358 @@ namespace DotJEM.Json.Storage.Linq
         public TResult Execute<TResult>(Expression expression)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public class Query<T> : IQueryable<T>, IQueryable, IEnumerable<T>, IEnumerable, IOrderedQueryable<T>, IOrderedQueryable
+    {
+        private readonly QueryProvider provider;
+        private readonly Expression expression;
+
+        public Query(QueryProvider provider)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+            this.provider = provider;
+            expression = Expression.Constant(this);
+        }
+
+        public Query(QueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+            if (!typeof(IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException("expression");
+            }
+            this.provider = provider;
+            this.expression = expression;
+        }
+
+        Expression IQueryable.Expression
+        {
+            get { return expression; }
+        }
+
+        Type IQueryable.ElementType
+        {
+            get { return typeof(T); }
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return provider; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)provider.Execute(expression)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)provider.Execute(expression)).GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return provider.GetQueryText(expression);
+        }
+    }
+
+    public abstract class QueryProvider : IQueryProvider
+    {
+        protected QueryProvider()
+        {
+        }
+
+        IQueryable<S> IQueryProvider.CreateQuery<S>(Expression expression)
+        {
+            return new Query<S>(this, expression);
+        }
+
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+            try
+            {
+                return (IQueryable)Activator.CreateInstance(typeof(Query<>).MakeGenericType(elementType), new object[] { this, expression });
+            }
+            catch (TargetInvocationException tie)
+            {
+                throw tie.InnerException;
+            }
+        }
+
+        S IQueryProvider.Execute<S>(Expression expression)
+        {
+            return (S)Execute(expression);
+        }
+
+        object IQueryProvider.Execute(Expression expression)
+        {
+            return Execute(expression);
+        }
+
+        public abstract string GetQueryText(Expression expression);
+        public abstract object Execute(Expression expression);
+    }
+
+    internal class QueryTranslator : ExpressionVisitor
+    {
+        private readonly string table;
+        private StringBuilder sb;
+
+        internal QueryTranslator(string table)
+        {
+            this.table = table;
+        }
+
+        internal string Translate(Expression expression)
+        {
+            sb = new StringBuilder();
+            Visit(expression);
+            return sb.ToString();
+        }
+
+        private static Expression StripQuotes(Expression e)
+        {
+            while (e.NodeType == ExpressionType.Quote)
+            {
+                e = ((UnaryExpression)e).Operand;
+            }
+            return e;
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression m)
+        {
+            if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Where")
+            {
+                sb.Append("SELECT * FROM (");
+                Visit(m.Arguments[0]);
+                sb.Append(") AS T WHERE ");
+                LambdaExpression lambda = (LambdaExpression)StripQuotes(m.Arguments[1]);
+                Visit(lambda.Body);
+                return m;
+            }
+            throw new NotSupportedException(string.Format("The method '{0}' is not supported", m.Method.Name));
+        }
+
+        protected override Expression VisitUnary(UnaryExpression u)
+        {
+            switch (u.NodeType)
+            {
+                case ExpressionType.Not:
+                    sb.Append(" NOT ");
+                    Visit(u.Operand);
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("The unary operator '{0}' is not supported", u.NodeType));
+            }
+            return u;
+        }
+
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            sb.Append("(");
+            Visit(b.Left);
+            switch (b.NodeType)
+            {
+                case ExpressionType.And:
+                    sb.Append(" AND ");
+                    break;
+                case ExpressionType.Or:
+                    sb.Append(" OR");
+                    break;
+                case ExpressionType.Equal:
+                    sb.Append(" = ");
+                    break;
+                case ExpressionType.NotEqual:
+                    sb.Append(" <> ");
+                    break;
+                case ExpressionType.LessThan:
+                    sb.Append(" < ");
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    sb.Append(" <= ");
+                    break;
+                case ExpressionType.GreaterThan:
+                    sb.Append(" > ");
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    sb.Append(" >= ");
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("The binary operator '{0}' is not supported", b.NodeType));
+            }
+            Visit(b.Right);
+            sb.Append(")");
+            return b;
+        }
+
+        protected override Expression VisitConstant(ConstantExpression c)
+        {
+            IQueryable q = c.Value as IQueryable;
+            if (q != null)
+            {
+                // assume constant nodes w/ IQueryables are table references
+                sb.Append("SELECT * FROM ");
+                sb.Append(table);
+            }
+            else if (c.Value == null)
+            {
+                sb.Append("NULL");
+            }
+            else
+            {
+                switch (Type.GetTypeCode(c.Value.GetType()))
+                {
+                    case TypeCode.Boolean:
+                        sb.Append(((bool)c.Value) ? 1 : 0);
+                        break;
+                    case TypeCode.String:
+                        sb.Append("'");
+                        sb.Append(c.Value);
+                        sb.Append("'");
+                        break;
+                    case TypeCode.Object:
+                        throw new NotSupportedException(string.Format("The constant for '{0}' is not supported", c.Value));
+                    default:
+                        sb.Append(c.Value);
+                        break;
+                }
+            }
+            return c;
+        }
+
+        protected Expression VisitMemberAccess(MemberExpression m)
+        {
+            if (m.Expression != null && m.Expression.NodeType == ExpressionType.Parameter)
+            {
+                sb.Append(m.Member.Name);
+                return m;
+            }
+            throw new NotSupportedException(string.Format("The member '{0}' is not supported", m.Member.Name));
+        }
+    }
+
+    internal class ObjectReader<T> : IEnumerable<T>, IEnumerable where T : class, new()
+    {
+        Enumerator enumerator;
+
+        internal ObjectReader(DbDataReader reader)
+        {
+            enumerator = new Enumerator(reader);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Enumerator e = enumerator;
+            if (e == null)
+            {
+                throw new InvalidOperationException("Cannot enumerate more than once");
+            }
+            enumerator = null;
+            return e;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class Enumerator : IEnumerator<T>, IEnumerator, IDisposable
+        {
+            DbDataReader reader;
+            FieldInfo[] fields;
+            int[] fieldLookup;
+            T current;
+
+            internal Enumerator(DbDataReader reader)
+            {
+                this.reader = reader;
+                fields = typeof(T).GetFields();
+            }
+
+            public T Current
+            {
+                get { return current; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return current; }
+            }
+
+            public bool MoveNext()
+            {
+                if (reader.Read())
+                {
+                    if (fieldLookup == null)
+                    {
+                        InitFieldLookup();
+                    }
+                    T instance = new T();
+                    for (int i = 0, n = fields.Length; i < n; i++)
+                    {
+                        int index = fieldLookup[i];
+                        if (index >= 0)
+                        {
+                            FieldInfo fi = fields[i];
+                            if (reader.IsDBNull(index))
+                            {
+                                fi.SetValue(instance, null);
+                            }
+                            else
+                            {
+                                fi.SetValue(instance, reader.GetValue(index));
+                            }
+                        }
+                    }
+                    current = instance;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+            }
+
+            public void Dispose()
+            {
+                reader.Dispose();
+            }
+
+            private void InitFieldLookup()
+            {
+                Dictionary<string, int> map = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+                for (int i = 0, n = reader.FieldCount; i < n; i++)
+                {
+                    map.Add(reader.GetName(i), i);
+                }
+                fieldLookup = new int[fields.Length];
+                for (int i = 0, n = fields.Length; i < n; i++)
+                {
+                    int index;
+                    if (map.TryGetValue(fields[i].Name, out index))
+                    {
+                        fieldLookup[i] = index;
+                    }
+                    else
+                    {
+                        fieldLookup[i] = -1;
+                    }
+                }
+            }
         }
     }
 
@@ -101,4 +527,47 @@ namespace DotJEM.Json.Storage.Linq
     //                   (typeof(T).Name == "IEnumerable`1"));
     //    }
     //}
+
+    internal static class TypeSystem
+    {
+        internal static Type GetElementType(Type seqType)
+        {
+            Type ienum = FindIEnumerable(seqType);
+            if (ienum == null) return seqType;
+            return ienum.GetGenericArguments()[0];
+        }
+
+        private static Type FindIEnumerable(Type seqType)
+        {
+            if (seqType == null || seqType == typeof(string))
+                return null;
+            if (seqType.IsArray)
+                return typeof(IEnumerable<>).MakeGenericType(seqType.GetElementType());
+            if (seqType.IsGenericType)
+            {
+                foreach (Type arg in seqType.GetGenericArguments())
+                {
+                    Type ienum = typeof(IEnumerable<>).MakeGenericType(arg);
+                    if (ienum.IsAssignableFrom(seqType))
+                    {
+                        return ienum;
+                    }
+                }
+            }
+            Type[] ifaces = seqType.GetInterfaces();
+            if (ifaces != null && ifaces.Length > 0)
+            {
+                foreach (Type iface in ifaces)
+                {
+                    Type ienum = FindIEnumerable(iface);
+                    if (ienum != null) return ienum;
+                }
+            }
+            if (seqType.BaseType != null && seqType.BaseType != typeof(object))
+            {
+                return FindIEnumerable(seqType.BaseType);
+            }
+            return null;
+        }
+    }
 }
