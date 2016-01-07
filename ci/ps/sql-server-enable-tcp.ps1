1b1f2f8cb39b85bca8d0aa3bc192e84ae6bf655d
Jens Melgaard <admin@it-links.dk>
2016-01-07 14:42:50 +0100

    Enable TCP

---
+++ ci/ps/sql-server-enable-tcp.ps1
@@ -0,0 +1,18 @@
+﻿[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
+
+$server = $env:COMPUTERNAME
+$instance = 'SQL2014'
+$smo = 'Microsoft.SqlServer.Management.Smo.'
+$wmi = new-object ($smo + 'Wmi.ManagedComputer')
+
+# Enable TCP/IP
+$uri = "ManagedComputer[@Name='$server']/ServerInstance[@Name='$instance']/ServerProtocol[@Name='Tcp']"
+$tcp = $wmi.GetSmoObject($uri)
+$tcp.IsEnabled = $true
+$tcp.alter()
+
+# Start services
+Set-Service SQLBrowser -StartupType Manual
+Start-Service SQLBrowser
+Start-Service "MSSQL`$$instance"
