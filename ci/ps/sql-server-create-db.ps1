162f26b8e849d4c76c63fdfc95d83d275e0a9204
Jens Melgaard <admin@it-links.dk>
2016-01-07 14:20:05 +0100

    List DB for debug

--- ci/ps/sql-server-create-db.ps1
+++ ci/ps/sql-server-create-db.ps1
@@ -1,7 +1,8 @@
 ﻿$path = "$($env:appveyor_build_folder)"
-$instance = ".\DEV"
+$instance = ".\SQL2014"
 
 # create new empty json database for tests.
 $mdf = join-path $path "json.mdf"
 $ldf = join-path $path "json.ldf"
 sqlcmd -S "$instance" -Q "Use [master]; CREATE DATABASE [json] ON ( NAME = json_dat, FILENAME = '$mdf' ) LOG ON ( NAME = json_log, FILENAME = '$ldf' )"
+sqlcmd -S "$instance" -Q "exec sp_databases" 
