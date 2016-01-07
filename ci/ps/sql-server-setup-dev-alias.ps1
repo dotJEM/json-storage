2a07663a7cc982876c8a915fbf22c6dad15c3bfe
Jens Melgaard <admin@it-links.dk>
2016-01-07 13:33:54 +0100

    try create databases

---
+++ ci/ps/sql-server-setup-dev-alias.ps1
@@ -0,0 +1,23 @@
+﻿[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
+
+$server = $env:COMPUTERNAME
+$instance = 'SQL2014'
+$smo = 'Microsoft.SqlServer.Management.Smo.'
+$wmi = new-object ($smo + 'Wmi.ManagedComputer')
+
+# Enable TCP/IP
+$uri = "ManagedComputer[@Name='$server']/ServerInstance[@Name='$instance']/ServerProtocol[@Name='Tcp']"
+$tcp = $wmi.GetSmoObject($uri)
+$tcp.IsEnabled = $true
+$tcp.alter()
+
+
+# Set Alias
+New-Item HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client -Name ConnectTo | Out-Null
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo -Name 'DEV' -Value "DBMSSOCN,$server\$instance" | Out-Null
+
+# Start services
+Set-Service SQLBrowser -StartupType Manual
+Start-Service SQLBrowser
+Start-Service "MSSQL`$$instance"
